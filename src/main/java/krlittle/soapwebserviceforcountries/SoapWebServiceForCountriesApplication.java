14b93f0adf17e835921b05da3600efe4787e0338
krlittle <krl49700@gmail.com>
2021-07-10 19:31:06 -0500

    init commit from start.spring.io

---
+++ src/main/java/krlittle/soapwebserviceforcountries/SoapWebServiceForCountriesApplication.java
@@ -0,0 +1,13 @@
+package krlittle.soapwebserviceforcountries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapWebServiceForCountriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapWebServiceForCountriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/krlittle/soapwebserviceforcountries/SoapWebServiceForCountriesApplicationTests.java
@@ -0,0 +1,13 @@
+package krlittle.soapwebserviceforcountries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapWebServiceForCountriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
